72705bee6616999b6c39693f8c717e798707af18
Edgar Ong <emailedgar@gmail.com>
2019-05-15 10:49:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(userName, userEmail){
+  // User
+  let userObject = {
+    name: userName,
+    email: userEmail
+  }
+
+  // Post
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userObject)
+  }
+
+  // Fetch
+  return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log("object: ", object)
+      document.body.innerHTML = object.id;
+    })
+    .catch(function(error) {
+      console.log("error: ", error.message)
+      document.body.innerHTML = error.message;
+    })
+}
