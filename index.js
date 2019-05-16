2d1679fade4ce305a0f66451c042b392efbee18c
ferryrules <ferris@ferryrules.com>
2019-05-16 17:13:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+let formData = {
+  name: "Steve",
+  email: "steve@steve.com"
+};
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+const bodyType = document.querySelector('#come-on')
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      bodyType.innerHTML += `<p>${object.id}</p>`
+    })
+    .catch(function(error) {
+      bodyType.innerHTML += "<p>Unauthorized Access</p>"
+    });
+}
